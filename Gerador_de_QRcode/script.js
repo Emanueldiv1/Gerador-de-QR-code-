32d3bb17e5339c54d928e7a14e489a31d157d870
emanuel <89155244+Emanueldiv1@users.noreply.github.com>
2023-03-18 09:02:10 -0300

    Add files via upload

---
+++ Gerador_de_QRcode/script.js
@@ -0,0 +1,27 @@
+const wrapper = document.querySelector(".wrapper"),
+qrInput = wrapper.querySelector(".form input"),
+generateBtn = wrapper.querySelector(".form button"),
+qrImg = wrapper.querySelector(".qr-code img");
+
+generateBtn.addEventListener("click", () => {
+    let qrValue = qrInput.value;
+    if(!qrValue)return; //se input estiver vazio ele retorna aki
+    generateBtn.innerText = "Gerador de QR Code....";
+    qrImg.src =`https://api.qrserver.com/v1/create-qr-code/?size=200x200&data=${qrValue}`; 
+    qrImg.addEventListener("load", () => {
+        wrapper.classList.add("active");
+        generateBtn.innerText = "Gerar QR Code";
+    });
+});
+
+qrInput.addEventListener("keyup", () => {
+    if(qrInput.value){
+        wrapper.classList.remove("active");
+    }
+});
+
+
+
+
+
+
